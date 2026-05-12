5f1fc42da153e90638e04ecb3f3b0bac6f369078
Gajapathy Dandu Rasamala <gajapathydandurasamala@Gajapathys-MacBook-Pro.local>
2026-05-13 00:45:55 +0530

    updated the file

--- playwright.config.js
+++ playwright.config.js
@@ -26,6 +26,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['html'],['line'],
   ['allure-playwright']],
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -33,7 +34,12 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    headless: true
+    headless: false,
+    launchOptions: {
+    args: ['--start-maximized'],
+    
+  },
+   screenshot: 'on'
   },
 
   /* Configure projects for major browsers */
